f6f1b648ec5e6f28d2d6d62a382d0d4b43dc8def
Ahmed Rayyan <ahmedhrayyan@outlook.com>
2020-03-16 01:29:50 +0200

    Add the logci which handle cart data
    Update ActionTypes; Create new actoin creator; create new reducer to
    handle shopping cart data

---
+++ src/data/CartActionCreators.js
@@ -0,0 +1,34 @@
+import { ActionTypes } from "./types";
+
+export function addToCard(product, quantity = 1) {
+  return {
+    type: ActionTypes.CART_ADD,
+    payload: {
+      product,
+      quantity
+    }
+  }
+}
+
+export function updateCartQuantity(product, quantity) {
+  return {
+    type: ActionTypes.CART_UPDATE,
+    payload: {
+      product,
+      quantity
+    }
+  }
+}
+
+export function removeFromCart(product) {
+  return {
+    type: ActionTypes.CART_REMOVE,
+    payload: product
+  }
+}
+
+export function clearCart() {
+  return {
+    type: ActionTypes.CART_CLEAR
+  }
+}
