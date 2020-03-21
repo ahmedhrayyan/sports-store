e3131454db0d17d38c5f45ce3ab9409e0b0b4345
Ahmed Rayyan <ahmedhrayyan@outlook.com>
2020-03-21 17:11:40 +0200

    Add Data Store Properties in ShopReducer.js

--- src/data/ShopReducer.js
+++ src/data/ShopReducer.js
@@ -5,7 +5,9 @@ export const ShopReducer = (storeData, action) => {
     case ActionTypes.DATA_LOAD:
       return {
         ...storeData,
-        [action.payload.dataType]: action.payload.data
+        [action.payload.dataType]: action.payload.data,
+        [`${action.payload.dataType}_total`]: action.payload.total,
+        [`${action.payload.dataType}_params`]: action.payload.params
       };
     default:
       return storeData || {};
