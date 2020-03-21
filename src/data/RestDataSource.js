3042f5f366fb7bb730af1b13387422b70378fe24
Ahmed Rayyan <ahmedhrayyan@outlook.com>
2020-03-21 17:08:11 +0200

    Add URL Parameter in the RestDataSource

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -1,6 +1,9 @@
 import Axios from "axios";
 import { RestUrls } from "./Urls";
 export class RestDataSource {
-  GetData = dataType => this.SendRequest("get", RestUrls[dataType]);
-  SendRequest = (method, url) => Axios.request({ method, url });
+  GetData = async(dataType, params) => {
+    return this.SendRequest("get", RestUrls[dataType], params)
+  }
+
+  SendRequest = (method, url, params) => Axios.request({ method, url, params });
 }
