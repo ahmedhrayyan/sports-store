6c12cb0282dd21bfc6d7a046cf105fe0e028db07
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-04-03 08:29:49 +0200

    Add StoreData method in RESTDataSource

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -5,5 +5,9 @@ export class RestDataSource {
     return this.SendRequest("get", RestUrls[dataType], params)
   }
 
-  SendRequest = (method, url, params) => Axios.request({ method, url, params });
+  StoreData = (dataType, data) => {
+    this.SendRequest("post", RestUrls[dataType], {}, data);
+  }
+
+  SendRequest = (method, url, params, data) => Axios.request({ method, url, params, data });
 }
