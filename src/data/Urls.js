bd4e074a10f35071dbf636d62fd327bc4ee2ddd5
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-04-07 10:54:25 +0200

    Add graphql url to URLS

--- src/data/Urls.js
+++ src/data/Urls.js
@@ -4,5 +4,7 @@ const hostname = "localhost";
 const port = 3500;
 export const RestUrls = {
   [DataTypes.PRODUCTS]: `${protocol}://${hostname}:${port}/api/products`,
-  [DataTypes.CATEGORIES]: `${protocol}://${hostname}:${port}/api/categories`
+  [DataTypes.CATEGORIES]: `${protocol}://${hostname}:${port}/api/categories`,
 };
+
+export const GraphQlUrl = `${protocol}://${hostname}:${port}/graphql`;
