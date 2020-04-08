68d46e26a29eb754ca1dad4e02dd390ab2c03851
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-04-08 20:36:30 +0200

    Update Routing Config in Admin.js

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -3,6 +3,11 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import { GraphQlUrl } from "../data/Urls";
 import { OrdersConnector } from "./OrdersConnector";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
+import { ConnectedProducts } from "./ProductsConnector";
+import { ProductEditor } from "./ProductEditor";
+import { ProductCreator } from "./ProductCreator";
 const graphQlClient = new ApolloClient({
   uri: GraphQlUrl,
 });
@@ -17,8 +22,21 @@ export class Admin extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col p-2">
-              <OrdersConnector />
+            <div className="col-3 p-2">
+              <ToggleLink to="/admin/orders">Orders</ToggleLink>
+              <ToggleLink to="/admin/products">Products</ToggleLink>
+            </div>
+            <div className="col-9 p-2">
+              <Switch>
+                <Route path="/admin/orders" component={OrdersConnector} />
+                <Route
+                  path="/admin/products/create"
+                  component={ProductCreator}
+                />
+                <Route path="/admin/products/:id" component={ProductEditor} />
+                <Route path="/admin/products" component={ConnectedProducts} />
+                <Redirect to="/admin/orders" />
+              </Switch>
             </div>
           </div>
         </div>
