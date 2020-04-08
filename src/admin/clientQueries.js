91167a9f60f0ad47bf02d27a2f7e19e7eb31da20
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-04-08 20:25:09 +0200

    Add more queries

--- src/admin/clientQueries.js
+++ src/admin/clientQueries.js
@@ -18,3 +18,29 @@ export const ordersSummaryQuery = gql`
     }
   }
 `;
+
+export const productsList = gql`
+  query($page: Int, $pageSize: Int, $sort: String) {
+    products {
+      totalSize
+      products(page: $page, pageSize: $pageSize, sort: $sort) {
+        id
+        name
+        category
+        price
+      }
+    }
+  }
+`;
+
+export const product = gql`
+  query($id: ID!) {
+    product(id: $id) {
+      id
+      name
+      description
+      category
+      price
+    }
+  }
+`;
