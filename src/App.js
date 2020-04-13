3493e95986c902bf4fa9ccd2fa42dbd6ac5a84f0
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-04-13 19:15:47 +0200

    Add Context Provider to App

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Redirect
+  Redirect,
 } from "react-router-dom";
 
 import { ShopConnector } from "./shop/ShopConnector";
 import { Admin } from "./admin/Admin";
+import { AuthProviderImpl } from "./auth/AuthProviderImpl";
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={SportsStoreDataStore}>
-        <Router>
-          <Switch>
-            <Route path="/shop" component={ShopConnector} />
-            <Route path="/admin" component={ Admin } />
-            <Redirect to="/shop" />
-          </Switch>
-        </Router>
+        <AuthProviderImpl>
+          <Router>
+            <Switch>
+              <Route path="/shop" component={ShopConnector} />
+              <Route path="/admin" component={Admin} />
+              <Redirect to="/shop" />
+            </Switch>
+          </Router>
+        </AuthProviderImpl>
       </Provider>
     );
   }
