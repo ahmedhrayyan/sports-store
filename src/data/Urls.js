b1ae42129773edc5e3e008237e7f257c21aae653
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-04-13 19:29:09 +0200

    Configure the Request URLs

--- src/data/Urls.js
+++ src/data/Urls.js
@@ -1,11 +1,12 @@
 import { DataTypes } from "./Types";
-const protocol = "http";
-const hostname = "localhost";
-const port = 3500;
-export const RestUrls = {
-  [DataTypes.PRODUCTS]: `${protocol}://${hostname}:${port}/api/products`,
-  [DataTypes.CATEGORIES]: `${protocol}://${hostname}:${port}/api/categories`,
-};
+// const protocol = "http";
+// const hostname = "localhost";
+// const port = 3500;
 
-export const GraphQlUrl = `${protocol}://${hostname}:${port}/graphql`;
-export const authUrl = `${protocol}://${hostname}:${port}/login`;
+export const RestUrls = {
+  [DataTypes.PRODUCTS]: `/api/products`,
+  [DataTypes.CATEGORIES]: `/api/categories`,
+  [DataTypes.ORDERS]: `/api/orders`,
+};
+export const GraphQlUrl = `/graphql`;
+export const authUrl = `/login`;
