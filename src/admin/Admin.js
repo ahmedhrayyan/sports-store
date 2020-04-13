02f9303fffea180469d645f09b11515455243b76
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-04-13 19:16:45 +0200

    Guard Admin features

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -8,39 +8,67 @@ import { ToggleLink } from "../ToggleLink";
 import { ConnectedProducts } from "./ProductsConnector";
 import { ProductEditor } from "./ProductEditor";
 import { ProductCreator } from "./ProductCreator";
-const graphQlClient = new ApolloClient({
-  uri: GraphQlUrl,
-});
-export class Admin extends Component {
-  render() {
-    return (
-      <ApolloProvider client={graphQlClient}>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col bg-info text-white">
-              <div className="navbar-brand">SPORTS STORE</div>
+
+import { AuthPrompt } from "../auth/AuthPrompt";
+import { authWrapper } from "../auth/AuthWrapper";
+// const graphQlClient = new ApolloClient({
+// uri: GraphQlUrl
+// });
+export const Admin = authWrapper(
+  class extends Component {
+    constructor(props) {
+      super(props);
+      this.client = new ApolloClient({
+        uri: GraphQlUrl,
+        request: (gqloperation) =>
+          gqloperation.setContext({
+            headers: {
+              Authorization: `Bearer<${this.props.webToken}>`,
+            },
+          }),
+      });
+    }
+    render() {
+      return (
+        <ApolloProvider client={this.client}>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col bg-info text-white">
+                <div className="navbar-brand">SPORTS STORE</div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-3 p-2">
+                <ToggleLink to="/admin/orders">Orders</ToggleLink>
+                <ToggleLink to="/admin/products">Products</ToggleLink>
+                {this.props.isAuthenticated && (
+                  <button
+                    onClick={this.props.signout}
+                    className="btn btn-block btn-secondary m-2 fixed-bottom col-3"
+                  >
+                    Log Out
+                  </button>
+                )}
+              </div>
+              <div className="col-9 p-2">
+                <Switch>
+                  {!this.props.isAuthenticated && (
+                    <Route component={AuthPrompt} />
+                  )}
+                  <Route path="/admin/orders" component={OrdersConnector} />
+                  <Route
+                    path="/admin/products/create"
+                    component={ProductCreator}
+                  />
+                  <Route path="/admin/products/:id" component={ProductEditor} />
+                  <Route path="/admin/products" component={ConnectedProducts} />
+                  <Redirect to="/admin/orders" />
+                </Switch>
+              </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col-3 p-2">
-              <ToggleLink to="/admin/orders">Orders</ToggleLink>
-              <ToggleLink to="/admin/products">Products</ToggleLink>
-            </div>
-            <div className="col-9 p-2">
-              <Switch>
-                <Route path="/admin/orders" component={OrdersConnector} />
-                <Route
-                  path="/admin/products/create"
-                  component={ProductCreator}
-                />
-                <Route path="/admin/products/:id" component={ProductEditor} />
-                <Route path="/admin/products" component={ConnectedProducts} />
-                <Redirect to="/admin/orders" />
-              </Switch>
-            </div>
-          </div>
-        </div>
-      </ApolloProvider>
-    );
+        </ApolloProvider>
+      );
+    }
   }
-}
+);
