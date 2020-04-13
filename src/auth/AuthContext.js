39fabf1d983424d498bbe579211624b26577f3d8
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-04-13 19:12:01 +0200

    Create AuthContext Component

---
+++ src/auth/AuthContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+export const AuthContext = React.createContext({
+  isAuthenticated: false,
+  webToken: null,
+  authenticate: (username, password) => {},
+  signout: () => {},
+});
